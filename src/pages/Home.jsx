ab05213ba727ded934acf4f057fdd257befb697a
Bugsbuny24 <99527234+Bugsbuny24@users.noreply.github.com>
2026-01-04 02:04:22 +0300

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,22 @@
 export default function Home() {
   return (
-    <div style={{ color: "white", padding: 40 }}>
+    <div
+      style={{
+        minHeight: "100vh",
+        background: "#020617",
+        color: "white",
+        padding: "40px",
+      }}
+    >
       <h1>TradePiGloball</h1>
       <p>Frontend çalışıyor 🚀</p>
+
+      <ul>
+        <li>/buyer/signup</li>
+        <li>/seller/signup</li>
+        <li>/buyer</li>
+        <li>/seller</li>
+      </ul>
     </div>
   );
 }
