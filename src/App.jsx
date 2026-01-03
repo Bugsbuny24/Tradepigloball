6468aca17c0766e5f8dc77dc6728818cec94a2be
Bugsbuny24 <99527234+Bugsbuny24@users.noreply.github.com>
2026-01-04 02:07:46 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,25 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import BuyerSignup from "./pages/BuyerSignup";
 import SellerSignup from "./pages/SellerSignup";
+import SellerWaiting from "./pages/SellerWaiting";
 import BuyerPanel from "./pages/BuyerPanel";
 import SellerPanel from "./pages/SellerPanel";
 
 export default function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/login" element={<Login />} />
 
-        <Route path="/buyer/signup" element={<BuyerSignup />} />
-        <Route path="/seller/signup" element={<SellerSignup />} />
+      <Route path="/buyer/signup" element={<BuyerSignup />} />
+      <Route path="/seller/signup" element={<SellerSignup />} />
 
-        <Route path="/buyer" element={<BuyerPanel />} />
-        <Route path="/seller" element={<SellerPanel />} />
-      </Routes>
-    </BrowserRouter>
+      <Route path="/buyer" element={<BuyerPanel />} />
+      <Route path="/seller" element={<SellerPanel />} />
+      <Route path="/seller/waiting" element={<SellerWaiting />} />
+    </Routes>
   );
 }
