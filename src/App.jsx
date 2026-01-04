c35a37a71158a548e272fe9931ef08780f3c09fe
Bugsbuny24 <99527234+Bugsbuny24@users.noreply.github.com>
2026-01-05 00:14:01 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import { Routes, Route } from "react-router-dom";
+
 import Home from "./pages/Home";
 import Login from "./pages/Login";
-import BuyerSignup from "./pages/BuyerSignup";
-import SellerSignup from "./pages/SellerSignup";
-import SellerWaiting from "./pages/SellerWaiting";
-import BuyerPanel from "./pages/BuyerPanel";
-import SellerPanel from "./pages/SellerPanel";
 
+import BuyerSignup from "./pages/BuyerSignup";
+
+// Company flow
+import CompanyApply from "./pages/CompanyApply";
+import CompanyWaiting from "./pages/CompanyWaiting";
+
+// Owner
 import PlatformOwnerRoute from "./components/PlatformOwnerRoute";
 import OwnerPanel from "./pages/OwnerPanel";
 
@@ -17,15 +20,14 @@ export default function App() {
       <Route path="/login" element={<Login />} />
 
       <Route path="/buyer/signup" element={<BuyerSignup />} />
-      <Route path="/seller/signup" element={<SellerSignup />} />
 
-      <Route path="/buyer" element={<BuyerPanel />} />
-      <Route path="/seller" element={<SellerPanel />} />
-      <Route path="/seller/waiting" element={<SellerWaiting />} />
+      {/* Company onboarding */}
+      <Route path="/company/apply" element={<CompanyApply />} />
+      <Route path="/company/waiting" element={<CompanyWaiting />} />
 
-      {/* Gizli Owner Panel (link yok, sadece URL ile) */}
+      {/* Owner panel */}
       <Route
-        path="/owner"
+        path="/panel"
         element={
           <PlatformOwnerRoute>
             <OwnerPanel />
