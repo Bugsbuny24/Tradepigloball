8a7409ad36cf718632a090b362714981c6fe0ff6
Bugsbuny24 <99527234+Bugsbuny24@users.noreply.github.com>
2026-01-04 08:31:51 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,10 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import BuyerSignup from "./pages/BuyerSignup";
 import SellerSignup from "./pages/SellerSignup";
 import SellerWaiting from "./pages/SellerWaiting";
-
 import BuyerPanel from "./pages/BuyerPanel";
 import SellerPanel from "./pages/SellerPanel";
 
@@ -14,29 +13,26 @@ import OwnerPanel from "./pages/OwnerPanel";
 
 export default function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
+    <Routes>
+      <Route path="/" element={<Home />} />
 
-        <Route path="/login" element={<Login />} />
+      <Route path="/login" element={<Login />} />
 
-        <Route path="/buyer/signup" element={<BuyerSignup />} />
-        <Route path="/seller/signup" element={<SellerSignup />} />
+      <Route path="/buyer/signup" element={<BuyerSignup />} />
+      <Route path="/seller/signup" element={<SellerSignup />} />
 
-        <Route path="/buyer" element={<BuyerPanel />} />
-        <Route path="/seller" element={<SellerPanel />} />
-        <Route path="/seller/waiting" element={<SellerWaiting />} />
+      <Route path="/buyer" element={<BuyerPanel />} />
+      <Route path="/seller" element={<SellerPanel />} />
+      <Route path="/seller/waiting" element={<SellerWaiting />} />
 
-        {/* GİZLİ OWNER PANEL */}
-        <Route
-          path="/owner"
-          element={
-            <PlatformOwnerRoute>
-              <OwnerPanel />
-            </PlatformOwnerRoute>
-          }
-        />
-      </Routes>
-    </BrowserRouter>
+      <Route
+        path="/owner"
+        element={
+          <PlatformOwnerRoute>
+            <OwnerPanel />
+          </PlatformOwnerRoute>
+        }
+      />
+    </Routes>
   );
 }
