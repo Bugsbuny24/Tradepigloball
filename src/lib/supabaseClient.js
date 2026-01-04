c819e5329780951e924e382071f3c465f33366c3
Bugsbuny24 <99527234+Bugsbuny24@users.noreply.github.com>
2026-01-04 19:38:06 +0300

    Update supabaseClient.js

--- src/lib/supabaseClient.js
+++ src/lib/supabaseClient.js
@@ -1,14 +1,6 @@
-import { createClient } from '@supabase/supabase-js';
+import { createClient } from "@supabase/supabase-js";
 
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
 const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
-
-// Vercel'de ENV yoksa build sırasında yakalamak için
-if (!supabaseUrl || !supabaseAnonKey) {
-  console.error(
-    'Missing VITE_SUPABASE_URL or VITE_SUPABASE_ANON_KEY. Set them in your .env file or Vercel environment variables'
-  );
-  throw new Error('Supabase credentials are missing!');
-}
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
