18914a55096ea1775a6ec264f2e6befc3ba9598a
Bugsbuny24 <99527234+Bugsbuny24@users.noreply.github.com>
2026-01-05 14:59:40 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,31 @@
-console.log("APP BOOTED");
-import { Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import Home from "./pages/Home";
 import Login from "./pages/Login";
-
-import BuyerSignup from "./pages/BuyerSignup";
-import BuyerPanel from "./pages/BuyerPanel";
-
-import CompanyApply from "./pages/CompanyApply";
-import CompanyWaiting from "./pages/CompanyWaiting";
+import OwnerPanel from "./pages/OwnerPanel";
 
 import PlatformOwnerRoute from "./components/PlatformOwnerRoute";
-import OwnerPanel from "./pages/OwnerPanel";
 
 export default function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/login" element={<Login />} />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
 
-      <Route path="/buyer/signup" element={<BuyerSignup />} />
-      <Route path="/buyer" element={<BuyerPanel />} />
+        <Route path="/login" element={<Login />} />
 
-      <Route path="/company/apply" element={<CompanyApply />} />
-      <Route path="/company/waiting" element={<CompanyWaiting />} />
+        <Route
+          path="/owner"
+          element={
+            <PlatformOwnerRoute>
+              <OwnerPanel />
+            </PlatformOwnerRoute>
+          }
+        />
 
-      {/* Owner link: /owner */}
-      <Route
-        path="/owner"
-        element={
-          <PlatformOwnerRoute>
-            <OwnerPanel />
-          </PlatformOwnerRoute>
-        }
-      />
-    </Routes>
+        {/* default */}
+        <Route path="*" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
