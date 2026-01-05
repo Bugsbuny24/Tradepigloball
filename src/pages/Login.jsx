d17a6673b3b4ac1c5615bd57193b1431e87805af
Bugsbuny24 <99527234+Bugsbuny24@users.noreply.github.com>
2026-01-05 20:06:41 +0300

    Update Login.jsx

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,9 +1,40 @@
+import { useState } from "react"
+import { supabase } from "../supabaseClient"
+
 export default function Login() {
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
+
+  const handleLogin = async () => {
+    const { error } = await supabase.auth.signInWithPassword({
+      email,
+      password,
+    })
+    if (error) alert(error.message)
+  }
+
   return (
-    <div>
-      Login
+    <div style={{ padding: 20 }}>
+      <h1>Login</h1>
+
+      <input
+        placeholder="Email"
+        value={email}
+        onChange={e => setEmail(e.target.value)}
+      />
+
+      <br /><br />
+
+      <input
+        type="password"
+        placeholder="Password"
+        value={password}
+        onChange={e => setPassword(e.target.value)}
+      />
+
+      <br /><br />
+
+      <button onClick={handleLogin}>Giriş Yap</button>
     </div>
   )
 }
-console.log("SUPABASE URL:", import.meta.env.VITE_SUPABASE_URL);
-console.log("SUPABASE KEY:", import.meta.env.VITE_SUPABASE_ANON_KEY);
