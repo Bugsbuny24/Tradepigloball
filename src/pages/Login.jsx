8d784bcedc98b6193d4d92ee2d38868799d724fe
Bugsbuny24 <99527234+Bugsbuny24@users.noreply.github.com>
2026-01-05 19:59:01 +0300

    Update Login.jsx

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,2 +1,9 @@
+export default function Login() {
+  return (
+    <div>
+      Login
+    </div>
+  )
+}
 console.log("SUPABASE URL:", import.meta.env.VITE_SUPABASE_URL);
 console.log("SUPABASE KEY:", import.meta.env.VITE_SUPABASE_ANON_KEY);
