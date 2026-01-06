38058becd5ec1b4804a88c7f2bd79bcd802a79a6
Bugsbuny24 <99527234+Bugsbuny24@users.noreply.github.com>
2026-01-06 11:14:16 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,23 @@
 import { Routes, Route } from "react-router-dom";
-
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
-
 import ProtectedRoute from "./components/ProtectedRoute";
 import RequireRole from "./components/RequireRole";
 
 import PiProducts from "./pages/PiProducts";
 import CreateRfq from "./pages/CreateRfq";
-import PiRfqs from "./pages/PiRfqs";
-import PiRfqDetail from "./pages/PiRfqDetail";
 import ExpoCity from "./pages/ExpoCity";
+import OwnerPanel from "./pages/OwnerPanel"; // <-- bunu ekle
 
 export default function App() {
   return (
     <Routes>
-      {/* Public */}
       <Route path="/" element={<Home />} />
+
       <Route path="/login" element={<Login />} />
       <Route path="/register" element={<Register />} />
 
-      {/* PI MODE */}
       <Route
         path="/pi/products"
         element={
@@ -40,34 +36,15 @@ export default function App() {
         }
       />
 
-      <Route
-        path="/pi/rfqs"
-        element={
-          <ProtectedRoute>
-            <PiRfqs />
-          </ProtectedRoute>
-        }
-      />
-
-      <Route
-        path="/pi/rfqs/:id"
-        element={
-          <ProtectedRoute>
-            <PiRfqDetail />
-          </ProtectedRoute>
-        }
-      />
-
-      {/* Expo */}
       <Route path="/expo-city" element={<ExpoCity />} />
 
-      {/* Admin */}
+      {/* OWNER PANEL */}
       <Route
         path="/admin"
         element={
           <ProtectedRoute>
             <RequireRole allow={["owner", "admin"]}>
-              <div style={{ padding: 24 }}>Admin Panel</div>
+              <OwnerPanel />
             </RequireRole>
           </ProtectedRoute>
         }
