24b967b4da0c499e9c2557f0b4b0935f87890906
Bugsbuny24 <99527234+Bugsbuny24@users.noreply.github.com>
2026-01-06 17:02:57 +0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
-import App from "./App.jsx";
+import App from "./App";
+import { SessionProvider } from "./lib/session";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+  <React.StrictMode>
+    <SessionProvider>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </SessionProvider>
+  </React.StrictMode>
 );
