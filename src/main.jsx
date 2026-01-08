b0f5fa4d3b5bba03f985d6909d1915b713763602
Bugsbuny24 <99527234+Bugsbuny24@users.noreply.github.com>
2026-01-08 21:26:22 +0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import { AuthProvider } from "./lib/auth.jsx";
-
+import { BrowserRouter } from "react-router-dom";
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <AuthProvider>
