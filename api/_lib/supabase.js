addc72c718f8c7bc90d78b6db85c9de67ee82c48
Bugsbuny24 <99527234+Bugsbuny24@users.noreply.github.com>
2026-01-10 14:40:57 +0300

    Update supabase.js

--- api/_lib/supabase.js
+++ api/_lib/supabase.js
@@ -6,15 +6,13 @@ export const supabaseUser = (req) => {
     process.env.SUPABASE_URL,
     process.env.SUPABASE_ANON_KEY,
     {
-      global: {
-        headers: token ? { Authorization: `Bearer ${token}` } : {}
-      }
+      auth: { persistSession: false },
+      global: { headers: token ? { Authorization: `Bearer ${token}` } : {} }
     }
   )
 }
 
 export const supabaseAdmin = () =>
-  createClient(
-    process.env.SUPABASE_URL,
-    process.env.SUPABASE_SERVICE_ROLE_KEY
-  )
+  createClient(process.env.SUPABASE_URL, process.env.SUPABASE_SERVICE_ROLE_KEY, {
+    auth: { persistSession: false }
+  })
