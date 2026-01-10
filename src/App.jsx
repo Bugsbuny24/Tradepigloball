38773f3df30afd86a8d4659787062d14ba0f04fb
Bugsbuny24 <99527234+Bugsbuny24@users.noreply.github.com>
2026-01-10 08:32:08 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -7,8 +7,7 @@ import Login from "./pages/Login";
 import Cart from "./pages/Cart";
 import Checkout from "./pages/Checkout";
 
-<Route path="/cart" element={<Cart />} />
-<Route path="/checkout" element={<Checkout />} />
+
 export default function App() {
   return (
     <>
@@ -18,9 +17,9 @@ export default function App() {
         <Route path="/products" element={<Products />} />
         <Route path="/login" element={<Login />} />
         <Route path="*" element={<Navigate to="/" replace />} />
-<Route path="/cart" element={<Cart />} />
+
+        <Route path="/cart" element={<Cart />} />
 <Route path="/checkout" element={<Checkout />} />
-        
       </Routes>
     </>
   );
