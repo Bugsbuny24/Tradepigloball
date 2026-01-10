0ae37aa31d93d8e06b710982b235632c384bb69f
Bugsbuny24 <99527234+Bugsbuny24@users.noreply.github.com>
2026-01-10 07:36:41 +0300

    Create Products.jsx

---
+++ src/pages/Products.jsx
@@ -0,0 +1,14 @@
+export default function Products() {
+  return (
+    <div style={{ padding: 20 }}>
+      <h1>Products</h1>
+      <p>Printify ürünleri burada listelenecek.</p>
+
+      <ul>
+        <li>BROKE Yazılı Tişört</li>
+        <li>Minimal Hoodie</li>
+        <li>TradePi Cap</li>
+      </ul>
+    </div>
+  );
+}
