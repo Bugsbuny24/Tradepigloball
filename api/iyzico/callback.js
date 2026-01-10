903b40a0a35f8e4d3c687d7d97bc84e7fc1463e4
Bugsbuny24 <99527234+Bugsbuny24@users.noreply.github.com>
2026-01-10 13:30:02 +0300

    Delete api/iyzico directory

--- api/iyzico/callback.js
+++
@@ -1,5 +0,0 @@
-export default async function handler(req, res) {
-  // iyzico callback burada ödeme sonucunu post eder.
-  // Burada: token ile retrieve yapıp (iyzico endpoint) status OK ise siparişi kaydedip Printify order yaratacağız.
-  return res.status(200).send("OK");
-}
