1b0b06afa15e1157608deaaf1c41a998b0025e0e
Bugsbuny24 <99527234+Bugsbuny24@users.noreply.github.com>
2026-01-10 07:32:26 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,24 +2,8 @@ import React from "react";
 import { Routes, Route, Navigate } from "react-router-dom";
 
 import NavBar from "./components/NavBar";
-
 import Home from "./pages/Home";
 import Login from "./pages/Login";
-
-import RFQs from "./pages/RFQs";
-import RFQDetail from "./pages/RFQDetail";
-
-import Orders from "./pages/Orders";
-import OrderDetail from "./pages/OrderDetail";
-import SellerOrders from "./pages/SellerOrders";
-import Billing from "./pages/Billing";
-import PiPayment from "./pages/PiPayment";
-
-import Products from "./pages/Products";
-import Store from "./pages/Store";
-import Stand from "./pages/Stand";
-
-import Credits from "./pages/Credits"; // ✅ SADECE BU!
 
 export default function App() {
   return (
@@ -28,27 +12,6 @@ export default function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
-
-        {/* RFQ */}
-        <Route path="/rfqs" element={<RFQs />} />
-        <Route path="/rfqs/:id" element={<RFQDetail />} />
-
-        {/* Orders */}
-        <Route path="/orders" element={<Orders />} />
-        <Route path="/orders/:id" element={<OrderDetail />} />
-
-        {/* Pi Payment */}
-        <Route path="/pi/payment/:orderId" element={<PiPayment />} />
-
-        {/* Products */}
-        <Route path="/pi/products" element={<Products />} />
-        <Route path="/store" element={<Store />} />
-        <Route path="/stand/:slug" element={<Stand />} />
-
-        {/* Credits */}
-        <Route path="/credits" element={<Credits />} />
-<Route path="/seller/orders" element={<SellerOrders />} />
-<Route path="/billing" element={<Billing />} />
         <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
     </>
