77c0036fef7de7107ae460a0b02ba95a97de0c95
Bugsbuny24 <99527234+Bugsbuny24@users.noreply.github.com>
2026-01-10 23:09:39 +0300

    Create supabase.js

---
+++ _lib/supabase.js
@@ -0,0 +1,15 @@
+import { createClient } from '@supabase/supabase-js'
+
+export function supabaseServer(req) {
+  return createClient(
+    process.env.VITE_SUPABASE_URL,
+    process.env.SUPABASE_SERVICE_ROLE_KEY,
+    {
+      global: {
+        headers: {
+          Authorization: req.headers.authorization || ''
+        }
+      }
+    }
+  )
+}
