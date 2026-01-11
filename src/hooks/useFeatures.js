2a0fcebc1b8bd17e374531ae862debfaab264fa8
Bugsbuny24 <99527234+Bugsbuny24@users.noreply.github.com>
2026-01-11 10:20:24 +0300

    Create useFeatures.js

---
+++ src/hooks/useFeatures.js
@@ -0,0 +1,10 @@
+export default function useFeatures() {
+  return {
+    rfq: true,
+    collab: true,
+    drop: true,
+    analytics: true,
+    ai: false,        // sonra açılır
+    localProducer: false
+  };
+}
