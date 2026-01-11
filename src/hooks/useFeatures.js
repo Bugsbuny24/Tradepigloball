825d696ffe5ca9c8ee3a1d83370f1ba6c446cbc8
Bugsbuny24 <99527234+Bugsbuny24@users.noreply.github.com>
2026-01-11 10:41:32 +0300

    Update useFeatures.js

--- src/hooks/useFeatures.js
+++ src/hooks/useFeatures.js
@@ -1,16 +1,12 @@
 export default function useFeatures() {
   return {
     support: true,
-    feature: true,
-    analytics: true,
-    collab: true,
-    drop: true,
-    boost: true,
     vote: true,
+    boost: true,
+    drop: true,
+    analytics: true,
     ai: true,
-
-    referral: true,        // v31
-    localProducer: true,   // v31
-    creatorBonus: true,    // v31
+    referral: true,
+    localProducer: true,
   };
 }
