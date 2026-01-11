1a7786acae0295f8d95d76c0132af417c180d84f
Bugsbuny24 <99527234+Bugsbuny24@users.noreply.github.com>
2026-01-11 10:56:56 +0300

    Update features.js

--- src/config/features.js
+++ src/config/features.js
@@ -1,12 +1,10 @@
-export const FEATURES = {
-  RFQ_SUPPORT: true,
-  RFQ_FEATURE: true,
-  RFQ_ANALYTICS: true,
-  COLLAB: true,
-  DROP: true,
-
-  AI_TITLE: false,
-  AI_FORECAST: false,
-  LOCAL_PRODUCER: false,
-  VOTING: false,
+export default {
+  support: true,
+  vote: true,
+  boost: true,
+  drop: true,
+  analytics: true,
+  ai: true,
+  referral: true,
+  localProducer: true,
 };
