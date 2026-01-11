8dc6101fd1f7ad9543e66a3ea0c619f4562daaa9
Bugsbuny24 <99527234+Bugsbuny24@users.noreply.github.com>
2026-01-11 14:39:14 +0300

    Update guard.js

--- god/guard.js
+++ god/guard.js
@@ -1,6 +1,7 @@
 import { ENV } from "../lib/env";
 
 export function isGod(user) {
+  if (ENV.APP_ENV === "production" && ENV.GOD_MODE !== true) return false;
   if (!ENV.GOD_MODE) return false;
   if (!user?.email) return false;
   return ENV.GOD_EMAILS.includes(user.email);
