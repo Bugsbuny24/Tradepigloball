c9ffa5df502f4c99f1f5f1c6ac539fca7b85ff10
Bugsbuny24 <99527234+Bugsbuny24@users.noreply.github.com>
2026-01-11 16:06:38 +0300

    Create index.jsx

---
+++ src/pages/admin/index.jsx
@@ -0,0 +1,11 @@
+export default function AdminDashboard() {
+  return (
+    <div>
+      <h1>Admin Panel</h1>
+
+      <a href="/admin/features">Features</a><br/>
+      <a href="/admin/system">System</a><br/>
+      <a href="/admin/audit">Audit Log</a>
+    </div>
+  );
+}
